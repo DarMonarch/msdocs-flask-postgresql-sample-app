79945db1bde923634ea0edbab4dbedb45706fd75
DarMonarch <sohailshaik9778@gmail.com>
2024-11-05 22:07:13 -0700

    Update development.py

--- azureproject/development.py
+++ azureproject/development.py
@@ -1,8 +1,8 @@
 import os
 
 DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=os.environ['DBUSER'],
-    dbpass=os.environ['DBPASS'],
-    dbhost=os.environ['DBHOST'],
-    dbname=os.environ['DBNAME']
+    dbuser=os.environ['myksbwyjfd'],
+    dbpass=os.environ['m$9smYMoRnwfcL4y'],
+    dbhost=os.environ['a1grpwebsite-server.postgres.database.azure.com'],
+    dbname=os.environ['a1grpwebsite-database']
 )
